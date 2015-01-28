4dde6b3b1d7cebf2eb17f7c2a68555f5553553aa
mustin98 <mustin98@hotmail.com>
2015-01-28 00:00:18 -0800

    testing geolocation

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,8 @@
 var time12 = true;
 var forecastKey = "2adeea29b1b55d8c635071be3f293285";
+var latitude;
+var longitude;
+
 
 function setTime12() {
    d = new Date();
@@ -50,30 +53,70 @@ function swapTimeModes() {
 function onLoad() {
    getTime();
    document.getElementById("button").setAttribute("onclick", "swapTimeModes()");
+   $("#forecastLabel").prepend("<p></p>");
    getTemp();
+   getLocation();
+}
+
+function getLocation() {
+   if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showLocationError);
+   } else {
+      $("#errorText").html("Geolocation is not supported by this browser");
+   }
+}
+
+function showPosition(position) {
+   $("#errorText").html("lat=" + position.coords.latitude + "<br>long=" + position.coords.longitude);
+}
+
+function showLocationError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            $("#errorText").html("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            $("#errorText").html("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            $("#errorText").html("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            $("#errorText").html("An unknown error occurred.");
+            break;
+    }
 }
 
 function getTemp() {
    $.getJSON("https://api.forecast.io/forecast/" + forecastKey + "/35.300399,-120.662362?callback=?", 
       function(data) {
-         $("#forecastLabel").html(data.daily.summary);
+         $("#forecastLabel p").html(data.daily.summary);
          $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png");
-         temp = data.daily.temperatureMax;
-         var tempClass
-         if (temp < 60) {
-            tempClass = "cold";
-         } else if (temp >= 60 && temp < 70) {
-            tempClass = "chilly";
-         } else if (temp >= 70 && temp < 80) {
-            tempClass = "nice";
-         } else if (temp>= 80  && temp < 90) {
-            tempClass = "warm";
-         } else {
-            tempClass = "hot";
-         }
 
-         $("body").attr("class", tempClass)
+         var temp;
+         $.each(data.daily.data, function(i, v) {
+            if (v.temperatureMax != undefined) {
+               temp = v.temperatureMax;
+            }
+         });
+
+         if (temp != undefined) {
+            var tempClass;
+            if (temp < 60) {
+               tempClass = "cold";
+            } else if (temp >= 60 && temp < 70) {
+               tempClass = "chilly";
+            } else if (temp >= 70 && temp < 80) {
+               tempClass = "nice";
+            } else if (temp>= 80  && temp < 90) {
+               tempClass = "warm";
+            } else {
+               tempClass = "hot";
+            }
+            $("body").attr("class", tempClass)
+         }
       });
+   setTimeout(getTemp, 30000);
 }
 
 $(document).ready(onLoad);
